8876d4afd0d73dd5a4e46b4ad69e54a09174026d
edno <corentin.edno.blondiau@gmail.com>
2022-12-03 05:26:48 +0100

    first commit

---
+++ scrap.py
@@ -0,0 +1,92 @@
+import requests
+import os
+from time import time, sleep
+
+api_url = "https://api.gtaliferp.fr:8443/v1/extinction/profiles/discord/304629682769494026"
+
+
+headers = {
+    'authority': 'api.gtaliferp.fr:8443',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'accept-language': 'fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7',
+    'cache-control': 'max-age=0',
+    'if-none-match': 'W/"924-RsfQlc+eIHVpM+alWwa0sGX4uP8"',
+    'sec-ch-ua': '"Google Chrome";v="107", "Chromium";v="107", "Not=A?Brand";v="24"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+    'sec-fetch-dest': 'document',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-site': 'none',
+    'sec-fetch-user': '?1',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36',
+}
+
+
+
+stats_dict = {
+    "zombie": 12,
+    "kill":9,
+    "death":11,
+    "ratio":29,
+    "zombie_redzone":23,
+    "kill_redzone":0,
+    "death_redzone":8,
+    "ratio_redzone":30,
+    "played_time":1
+}
+
+def time_conversion(playtime): ## askpython.com
+    sec_value = playtime % (24 * 3600)
+    hour_value = sec_value // 3600
+    sec_value %= 3600
+    minutes = sec_value // 60
+    sec_value %= 60
+
+    if minutes < 10:
+        time = f"{hour_value}:0{minutes}"
+    else:
+        time = f"{hour_value}:{minutes}"
+
+    print(f"{time : <10} 🕒 Temps de jeu")
+
+def main():
+
+
+    response = requests.get(api_url, headers=headers)
+    data = response.json()
+
+    hour_value = 0
+    minutes = 0
+
+
+
+    zombie_kills = data["stats"][stats_dict["zombie"]]["value"]
+    pvp_kills = data["stats"][stats_dict["kill"]]["value"]
+    deaths = data["stats"][stats_dict["death"]]["value"]
+    ratio = data["stats"][stats_dict["ratio"]]["value"]
+    zombie_redzone = data["stats"][stats_dict["zombie_redzone"]]["value"]
+    kills_redzone = data["leaderboard"][stats_dict["kill_redzone"]]["value"]
+    deaths_redzone = data["stats"][stats_dict["death_redzone"]]["value"]
+    ratio_redzone = data["stats"][stats_dict["ratio_redzone"]]["value"]
+    playtime = data["stats"][stats_dict["played_time"]]["value"]
+    level = data["rank"]
+
+
+    print(f"{zombie_kills : <10} 🧟 Zombies tués")
+    print(f"{pvp_kills : <10} ⚔️ Opps tués")
+    print(f"{deaths : <10} 🩸 Morts")
+    print(f"{ratio : <10} 〽 Ratio")
+    print(f"{zombie_redzone : <10} 🧟🔴 Zombies tués en redzone")
+    print(f"{kills_redzone : <10} ⚔️🔴 Opps tués en redzone")
+    print(f"{deaths_redzone : <10} 🩸🔴 Morts en redzone")
+    print(f"{ratio_redzone : <10} 〽🔴 Ratio en redzone")
+    print(f"{level : <10} 👾 Niveau")
+
+    time_conversion(playtime)
+
+while True:
+
+    os.system('cls||clear') # Clears terminal for readability.
+    main()
+    sleep(20 - time() % 20)
